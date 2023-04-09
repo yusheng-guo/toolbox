d07df31e0e81f8ae414730c72b0153be1cf29d2d
unknown <yushengguo557@gmail.com>
2023-04-09 22:58:14 +0800

    IsPrime Function.

---
+++ calculate/isPrime.go
@@ -0,0 +1,13 @@
+package calculate
+
+func IsPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ calculate/isPrime_test.go
@@ -0,0 +1,11 @@
+package calculate
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsPrime(t *testing.T) {
+	fmt.Println(IsPrime(10))
+	fmt.Println(IsPrime(7))
+}
